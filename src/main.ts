cd205694fd5c901a1f6cb901e6ccfb0840b6acf2
z20ist01 <z20ist01@z00p3-13.SPSKNM>
2022-10-18 09:23:17 +0200

    18.10

--- src/main.ts
+++ src/main.ts
@@ -11,3 +11,18 @@ fetch('https://movies-app1.p.rapidapi.com/api/genres', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
+	
+	document.getElementById('app').innerHTML= 
+`<header>
+	<div class="container">
+		<span class="main-title">
+			Movies
+		</span>
+		
+		<span id="form">
+			<input type="search" id="query" class="q" placeholder="Search...">
+			<button class= "butt"> Search </button>
+		</span>
+	</div>
+
+</header>`
