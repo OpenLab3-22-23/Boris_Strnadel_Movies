52913c1d946e3b78ff9ae9c097c7f218db285313
CurrentlyOnline <sambor433@gmail.com>
2022-10-26 18:49:08 +0200

    done

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,70 @@
-import './style.css'
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '8c9fb26915mshb60c79b6006990bp13d41fjsn7b4e4e9d7830',
-		'X-RapidAPI-Host': 'movies-app1.p.rapidapi.com'
-	}
-};
+import './style.css';
+const APIURL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI =
+  "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
-fetch('https://movies-app1.p.rapidapi.com/api/genres', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-	
-	document.getElementById('app').innerHTML= 
-`<header>
-	<div class="container">
+  
+
+
+  document.getElementById('app')!.innerHTML = `
+  <body>
+    <header>
+    <form id="form">
+        <input type="text" id="search" placeholder="Search" class="search" />
 		<span class="main-title">
 			Movies
-		</span>
-		
-		<span id="form">
-			<input type="search" id="query" class="q" placeholder="Search...">
-			<button class= "butt"> Search </button>
-		</span>
-	</div>
+	</span>
+    </form>
+    </header>
+    <main id="main"></main>
+  </body>
+</html>
+`;
 
-</header>`
+
+
+
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+const getMovies = async (url) => {
+  const resp = await fetch(url);
+  const respData = await resp.json();
+
+  console.log(respData);
+
+  showMovies(respData.results);
+};
+
+const showMovies = (movies) => {
+	main.innerHTML = "";
+  
+	movies.forEach((movie) => {
+	  const { poster_path, title} = movie;
+  
+	  const movieEl = document.createElement("div");
+	  movieEl.classList.add("movie");
+  
+	  movieEl.innerHTML = `
+		<img
+		src="${IMGPATH + poster_path}"
+		alt="${title}"
+		/>`; 
+		main.appendChild(movieEl);
+	});
+  };
+
+ 
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	const searchTerm = search.value;
+  
+	if (searchTerm) {
+	  getMovies(SEARCHAPI + searchTerm);
+	  search.value = "";
+	}
+  });
